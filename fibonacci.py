772f4e327e521b5eea4e812bb27ca62a7cc09b69
Abhi120806-hub <abhiop1206@gmail.com>
2026-09-07 09:18:32 +0530

    Add files via upload
    Created two different programs.
    1) To calculate the fibonacci series, till the number the user wishes
    2) To maintain my details related to my university

---
+++ fibonacci.py
@@ -0,0 +1,6 @@
+a =0
+b=1
+n = int(input("Enter the number till where you want the fibonacci series"))
+for i in range(n):
+    print(a,end=" ")
+    a,b=b,a+b
---
+++ struct.py
@@ -0,0 +1,4 @@
+details = dict()
+details = {"Name":"Abhi","Rollno":2853,"Section":"I"}
+for key,values in details.items():
+    print(key,values)
